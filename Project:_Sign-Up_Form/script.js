c03d19f0c20531ecfb3091f25cb8c755d8ae3e85
Stary_Algida <piotrwc49@gmail.com>
2023-06-13 19:43:11 +0200

    js added

---
+++ Project:_Sign-Up_Form/script.js
@@ -0,0 +1,28 @@
+const allInputs = document.querySelectorAll("input");
+const password1 = document.querySelector("#password");
+const password2 = document.querySelector("#rpassword");
+
+
+allInputs.forEach(element => {
+    element.addEventListener("focus", () =>{
+        element.style.borderColor = null;
+    });
+
+    if(element.value === ""){
+        element.style.borderColor = "#e5e7eb";
+    }
+});
+
+
+password2.addEventListener("focusout", () =>{
+    if(password2.value !== password1.value){
+        password2.style.borderColor = "red";
+        password1.style.borderColor = "red";
+        console.log("ok");
+    }
+    else{
+        password2.style.borderColor = null;
+        password1.style.borderColor = null;
+    }
+})
+
